70e053988edf84d7a9eb8baff5ffaebf0a5e51ef
Laura Quixabeira Mendes <lauraquixabeira@hotmail.com>
2022-06-20 11:15:32 -0300

    lectures and challenges fundamentals part 1 and 2

--- part_2/lectures2.js
+++ part_2/lectures2.js
@@ -124,3 +124,92 @@
 // change the array at that index position. For example, you can search for
 // 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
 
+
+// const neighbours = ['Venezuela', 'Brazil', 'Paraguay'];
+// neighbours.push('Utopia');
+// console.log(neighbours);
+
+// neighbours.pop();
+// console.log(neighbours);
+
+// if(neighbours.includes('Germany')){
+//     console.log('It is a central European country');
+// }else{
+//     console.log('Probably not a central European country')
+// }
+
+
+// neighbours[neighbours.indexOf('Brazil')]= 'Republic of Brazil';
+// console.log(neighbours);
+
+//////////////////////////////////////////////
+// LECTURE: Introduction to Objects
+// 1. Create an object called 'myCountry' for a country of your choice,
+//  containing
+// properties 'country', 'capital', 'language', 'population' and
+// 'neighbours' (an array like we used in previous assignments)
+
+// const myCountry = {
+// country: 'Brazil',
+// capital:'Brazilia',
+// language: 'Portuguese',
+// population: 1000000000,
+// neighbours: ['Paraguay', 'Uruguai', 'Bolivia', 'Peru' ]
+// };
+// console.log(myCountry);
+
+//////////////////////////////////////////////
+// LECTURE: Dot vs. Bracket Notation
+// 1. Using the object from the previous assignment, log a string like this to the
+// console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+// and a capital called Helsinki.'
+// 2. Increase the country's population by two million using dot notation, and then
+// decrease it by two million using brackets notation.
+
+
+// const y = {
+// country: 'Finland',
+// capital:'Helsinki',
+// language: 'finnish',
+// population: 6,
+// neighbours: ['Sweden', 'Russia', 'Norway']
+// };
+
+// console.log(`${y.country} has ${y.population} ${y.language}-speaking people, ${y.neighbours.length} neighbouring countries and a capital called ${y.capital}`);
+
+// y.population += 2; 
+// console.log(y.population);
+// y['population'] -= 2;
+// console.log(y.population);
+
+//////////////////////////////////////////////
+//LECTURE: Object Methods
+// 1. Add a method called 'describe' to the 'myCountry' object. This method
+// will log a string to the console, similar to the string logged in the previous
+// assignment, but this time using the 'this' keyword.
+// 2. Call the 'describe' method
+// 3. Add a method called 'checkIsland' to the 'myCountry' object. This
+// method will set a new property on the object, called 'isIsland'.
+// 'isIsland' will be true if there are no neighbouring countries, and false if
+// there are. Use the ternary operator to set the property.
+
+// const myCountry = {
+//     country: 'Brazil',
+//     capital:'Brazilia',
+//     language: 'Portuguese',
+//     population: 1000000000,
+//     neighbours: ['Paraguay', 'Uruguai', 'Bolivia', 'Peru' ], 
+   
+//     describe: function(){
+// return `${this.country} has ${this.population} ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`
+//     },
+
+
+// checkIsland: function(){
+// return `${this.country} has ${this.neighbours.lenght === 4 ? 'true': 'false'}`
+
+// }
+// }
+
+// console.log(myCountry.describe());
+// console.log(myCountry.checkIsland());
